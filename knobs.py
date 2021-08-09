91f27414e04747ee8210ac32a3a8fe8f5b5d9ca8
jsw166098 <jsw166098@gmail.com>
2021-08-10 06:05:14 +0900

    lightgbm

---
+++ knobs.py
@@ -0,0 +1,34 @@
+knobs = {
+    "appendonly": None,
+    "appendfsync": None,
+    "auto-aof-rewrite-percentage": None,
+    "auto-aof-rewrite-min-size": None,
+    "no-appendfsync-on-rewrite": None,
+    "aof-rewrite-incremental-fsync": None,
+    "aof-use-rdb-preamble": None,
+
+    # "save": None,
+    "rdbcompression": None,
+    "rdbchecksum": None,
+    "rdb-save-incremental-fsync": None,
+
+    "activedefrag": None,
+    "active-defrag-threshold-lower": None,
+    "active-defrag-threshold-upper": None,
+    "active-defrag-cycle-min": None,
+    "active-defrag-cycle-max": None,
+
+    "maxmemory": None,
+    "maxmemory-policy": None,
+    "maxmemory-samples": None,
+    "lazyfree-lazy-eviction": None,
+    "lazyfree-lazy-expire": None,
+    "lazyfree-lazy-server-del": None,
+
+    "hash-max-ziplist-entries": None,
+    "hash-max-ziplist-value": None,
+    "activerehashing": None,
+    "hz": None,
+    "dynamic-hz": None
+}
+
